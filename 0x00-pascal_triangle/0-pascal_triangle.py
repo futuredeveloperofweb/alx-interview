8969fc50bdfc18069c2dc767c5bbf60fcdc180bd
othmane <othmane.alaouimekkaoui@gmail.com>
2024-04-25 22:49:14 +0000

    Pascal Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Pascal's Triangle code'''
+
+
+def pascal_triangle(n):
+    '''returns a list of lists of integers representing the
+    Pascal’s triangle of n'''
+    triangle = []
+
+    if n <= 0:
+        return triangle
+    for i in range(n):
+        temp = []
+
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp.append(1)
+            else:
+                temp.append(triangle[i-1][j-1] + triangle[i-1][j])
+        triangle.append(temp)
+
+    return triangle
---
+++ README.md
@@ -0,0 +1,1 @@
+This repo is for solving problems similars to the ones we can have during an interview in different languages
