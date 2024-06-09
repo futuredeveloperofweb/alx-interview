dfcf07e0474f834761776bc89cd7904e1389e94a
othmane <othmane.alaouimekkaoui@gmail.com>
2024-06-09 16:08:11 -0700

    tasks

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,31 +1,49 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 
 const request = require('request');
 
 const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
+
 const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-function sendRequest (characterList, index) {
-  if (characterList.length === index) {
+function fetchCharacterNames(characterList, index) {
+  if (index >= characterList.length) {
     return;
   }
 
   request(characterList[index], (error, response, body) => {
     if (error) {
-      console.log(error);
+      console.error('Error fetching character:', error);
+    } else if (response.statusCode !== 200) {
+      console.error(`Error: Received status code ${response.statusCode}`);
     } else {
-      console.log(JSON.parse(body).name);
-      sendRequest(characterList, index + 1);
+      try {
+        const character = JSON.parse(body);
+        console.log(character.name);
+      } catch (parseError) {
+        console.error('Error parsing character JSON:', parseError);
+      }
     }
+    fetchCharacterNames(characterList, index + 1);
   });
 }
 
 request(movieEndpoint, (error, response, body) => {
   if (error) {
-    console.log(error);
+    console.error('Error fetching movie:', error);
+  } else if (response.statusCode !== 200) {
+    console.error(`Error: Received status code ${response.statusCode}`);
   } else {
-    const characterList = JSON.parse(body).characters;
-
-    sendRequest(characterList, 0);
+    try {
+      const movie = JSON.parse(body);
+      const characterList = movie.characters;
+      fetchCharacterNames(characterList, 0);
+    } catch (parseError) {
+      console.error('Error parsing movie JSON:', parseError);
+    }
   }
 });
