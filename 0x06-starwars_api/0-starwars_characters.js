2628c2978f0ec7d2397e9ff7d2912fc44b36963b
othmane <othmane.alaouimekkaoui@gmail.com>
2024-06-09 15:57:35 -0700

    tasks

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,26 +2,45 @@
 
 const request = require('request');
 
-const req = (arr, i) => {
-  if (i === arr.length) return;
-  request(arr[i], (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      console.log(JSON.parse(body).name);
-      req(arr, i + 1);
-    }
-  });
-};
+if (process.argv.length !== 3) {
+  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
 
-request(
-  `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
-  (err, response, body) => {
-    if (err) {
-      throw err;
-    } else {
-      const chars = JSON.parse(body).characters;
-      req(chars, 0);
-    }
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+request(url, { json: true }, (err, res, body) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
   }
-);
+
+  if (res.statusCode !== 200) {
+    console.error(`Error: ${res.statusCode}`);
+    process.exit(1);
+  }
+
+  const characters = body.characters;
+  if (!characters) {
+    console.error('No characters found');
+    process.exit(1);
+  }
+
+  characters.forEach(characterUrl => {
+    request(characterUrl, { json: true }, (err, res, body) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+
+      if (res.statusCode !== 200) {
+        console.error(`Error: ${res.statusCode}`);
+        process.exit(1);
+      }
+
+      console.log(body.name);
+    });
+  });
+});
+
