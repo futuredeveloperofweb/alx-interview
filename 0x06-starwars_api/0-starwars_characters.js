610e9de579a0f9775dcdf8e31bb79722c4c19f9d
othmane <othmane.alaouimekkaoui@gmail.com>
2024-06-09 16:03:00 -0700

    tasks

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,45 +2,30 @@
 
 const request = require('request');
 
-if (process.argv.length !== 3) {
-  console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-  process.exit(1);
+const movieId = process.argv[2];
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
+
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
+  });
 }
 
-const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
+request(movieEndpoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
 
-request(url, { json: true }, (err, res, body) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
+    sendRequest(characterList, 0);
   }
-
-  if (res.statusCode !== 200) {
-    console.error(`Error: ${res.statusCode}`);
-    process.exit(1);
-  }
-
-  const characters = body.characters;
-  if (!characters) {
-    console.error('No characters found');
-    process.exit(1);
-  }
-
-  characters.forEach(characterUrl => {
-    request(characterUrl, { json: true }, (err, res, body) => {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-
-      if (res.statusCode !== 200) {
-        console.error(`Error: ${res.statusCode}`);
-        process.exit(1);
-      }
-
-      console.log(body.name);
-    });
-  });
 });
-
